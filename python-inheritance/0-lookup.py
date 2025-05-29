27cb051cd9a3a7ee246af1bcf3cea627b475627a
Baxdr <10670@holbertonstudents.com>
2025-05-29 20:11:23 +0300

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Lookup function that returns the list of available attributes and methods of an object."""
+
+
 def lookup(obj):
+    """Returns a list of available attributes and methods of an object."""
     return list(obj)
