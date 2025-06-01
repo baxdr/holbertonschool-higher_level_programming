726b1b1129beb486613ecdfdb509e004f3d354c3
Baxdr <10670@holbertonstudents.com>
2025-06-01 22:16:18 +0300

    task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -57,3 +57,15 @@ def shape_info(shape):
     """
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
+
+
+if __name__ == "__main__":
+    circle = Circle(5)
+    rectangle = Rectangle(4, 7)
+
+    print("Circle:")
+    shape_info(circle)  # Should print the area and perimeter of the circle
+
+    print("\nRectangle:")
+    # Should print the area and perimeter of the rectangle
+    shape_info(rectangle)
