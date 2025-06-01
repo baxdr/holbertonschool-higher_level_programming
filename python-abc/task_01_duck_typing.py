807f139395009b9696c4a2572d3e8795b1deb608
Baxdr <10670@holbertonstudents.com>
2025-06-01 21:27:13 +0300

    task 1

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This module demonstrates the use of duck typing in Python.
+"""
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    """
+    Abstract base class for shapes
+    """
+    @abstractmethod
+    def area(self):
+        pass
+
+    def perimeter(self):
+        """
+        Default implementation for perimeter, can be overridden
+        """
+        raise NotImplementedError(
+            "This shape does not have a defined perimeter method")
+
+
+class Circle(Shape):
+    """
+    Concrete class for Circle that implements the Shape interface
+    """
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+
+class Rectangle(Shape):
+    """
+    Concrete class for Rectangle that implements the Shape interface
+    """
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
