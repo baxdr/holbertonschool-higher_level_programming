053e4190dfdb9ac3d82f2b309233938560aa9be2
Bader <10670@holbertonstudents.com>
2025-06-12 11:57:31 +0300

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a function that reads a text file and prints it to stdout."""
+d = __import__("0-read_file").__doc__
+
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
