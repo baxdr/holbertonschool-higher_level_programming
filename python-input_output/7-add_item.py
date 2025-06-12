52473128613eeaf57d6c3b55e546e942eccc209d
Bader <10670@holbertonstudents.com>
2025-06-12 13:02:04 +0300

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,8 +4,8 @@ script to save and load
 """
 import sys
 
-save_json = __import__('7-save_to_json_file').save_to_json_file
-load_json = __import__('8-load_from_json_file').load_from_json_file
+save_json = __import__('5-save_to_json_file').save_to_json_file
+load_json = __import__('6-load_from_json_file').load_from_json_file
 
 file = "add_item.json"
 try:
